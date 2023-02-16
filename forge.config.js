c376f4c2ce8594f85d6752a0da82629e2189d208
Nathan Totten <nathan@ntotten.com>
2023-02-15 20:18:43 -0500

    updated build

---
+++ forge.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  packagerConfig: {},
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "zuplo",
+          name: "image-uploader",
+        },
+        prerelease: false,
+      },
+    },
+  ],
+  makers: [
+    {
+      name: "@electron-forge/maker-dmg",
+    },
+    // {
+    //   name: "@electron-forge/maker-pkg",
+    // },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+  ],
+};
